f61c8ef87ec2495169dd06026c32a18141726e06
Jyc516 <jxxx17jyc@163.com>
2025-10-27 22:33:26 +0800

    feat: - add a enum for switches - add a linear_mapping for channels

--- Core/Inc/RemoteController.h
+++ Core/Inc/RemoteController.h
@@ -19,12 +19,22 @@ private:
   static constexpr int ticks_timeout = 1000;
   bool connection_status = true;
 
-  int CH0 = 1024;     // 364 - 1024 - 1684
-  int CH1 = 1024;     // 364 - 1024 - 1684
-  int CH2 = 1024;     // 364 - 1024 - 1684
-  int CH3 = 1024;     // 364 - 1024 - 1684
-  int S1 = 1;         // 1 - 3
-  int S2 = 1;         // 1 - 3
+  static constexpr int CH_low = 364;
+  static constexpr int CH_mid = 1024;
+  static constexpr int CH_high = 1684;
+
+  enum SwitchState {
+    UP = 1,
+    DOWN = 2,
+    MID = 3
+  };
+
+  float CH0 = 0;     // 364 - 1024 - 1684
+  float CH1 = 0;     // 364 - 1024 - 1684
+  float CH2 = 0;     // 364 - 1024 - 1684
+  float CH3 = 0;     // 364 - 1024 - 1684
+  SwitchState S1 = MID;         // 1 - 3
+  SwitchState S2 = MID;         // 1 - 3
   int axis_X = 0;          // -32768 - 0 - 32767
   int axis_Y = 0;          // -32768 - 0 - 32767
   int axis_Z = 0;          // -32768 - 0 - 32767
@@ -38,6 +48,10 @@ private:
   int key_E = 0;      // 0 - 1
   int key_SHIFT = 0;  // 0 - 1
   int key_CTRL = 0;   // 0 - 1
+
+  static float linear_mapping(int raw, int raw_low, int raw_high, float res_low, float res_high) {
+    return float(raw - raw_low) / float(raw_high - raw_low) * (res_high - res_low) + res_low;
+  }
 public:
   uint8_t rx_buf[32] = {0};
 
--- Core/Src/RemoteController.cpp
+++ Core/Src/RemoteController.cpp
@@ -13,37 +13,30 @@ void RC::handle(){
   for (int i = 0; i < 18; ++i) {
     rx_data[i] = rx_buf[i];
   }
-  CH0 = (rx_data[0] | rx_data[1] << 8) & 0x07FF;
-  // CH1 = (rx_data[1] >> 3 | rx_data[2] << 5) & 0x07FF;
-  // CH2 = (rx_data[2] >> 6 | rx_data[3] << 2 | rx_data[4] << 10) & 0x07FF;
-  // CH3 = (rx_data[4] >> 1 | rx_data[5] << 7) & 0x07FF;
-  // S1 = (rx_data[5] >> 4) & 0x03;
-  // S2 = (rx_data[5] >> 6) & 0x03;
-  // axis_X = static_cast<int16_t>(rx_data[6] | rx_data[7] << 8);
-  // axis_Y = static_cast<int16_t>(rx_data[8] | rx_data[9] << 8);
-  // axis_Z = static_cast<int16_t>(rx_data[10] | rx_data[11] << 8);
-  // left = rx_data[12] & 0x01;
-  // right = rx_data[13] & 0x01;
-  // key_W = (rx_data[14] >> 7) & 0x01;
-  // key_S = (rx_data[14] >> 6) & 0x01;
-  // key_A = (rx_data[14] >> 5) & 0x01;
-  // key_D = (rx_data[14] >> 4) & 0x01;
-  // key_Q = (rx_data[14] >> 3) & 0x01;
-  // key_E = (rx_data[14] >> 2) & 0x01;
-  // key_SHIFT = (rx_data[14] >> 1) & 0x01;
-  // key_CTRL = rx_data[14] & 0x01;
+  int CH_tmp;
 
-  // CH0 = (rx_data[0] << 3 | rx_data[1] >> 5) & 0x07FF;
-  CH1 = (rx_data[1] >> 3 | rx_data[2] << 5) & 0x07FF;
-  CH2 = (rx_data[2] >> 6 | rx_data[3] << 2 | rx_data[4] << 10) & 0x07FF;
-  CH3 = (rx_data[4] >> 1 | rx_data[5] << 7) & 0x07FF;
-  S1 = (rx_data[5] >> 4) & 0x03;
-  S2 = (rx_data[5] >> 6) & 0x03;
+  CH_tmp = (rx_data[0] | rx_data[1] << 8) & 0x07FF;
+  CH0 = linear_mapping(CH_tmp, CH_low, CH_high, -1.0f, 1.0f);
+
+  CH_tmp = (rx_data[1] >> 3 | rx_data[2] << 5) & 0x07FF;
+  CH1 = linear_mapping(CH_tmp, CH_low, CH_high, -1.0f, 1.0f);
+
+  CH_tmp = (rx_data[2] >> 6 | rx_data[3] << 2 | rx_data[4] << 10) & 0x07FF;
+  CH2 = linear_mapping(CH_tmp, CH_low, CH_high, -1.0f, 1.0f);
+
+  CH_tmp = (rx_data[4] >> 1 | rx_data[5] << 7) & 0x07FF;
+  CH3 = linear_mapping(CH_tmp, CH_low, CH_high, -1.0f, 1.0f);
+
+  S1 = static_cast<SwitchState>((rx_data[5] >> 4) & 0x03);
+  S2 = static_cast<SwitchState>((rx_data[5] >> 6) & 0x03);
+
   axis_X = static_cast<int16_t>(rx_data[6] | rx_data[7] << 8);
   axis_Y = static_cast<int16_t>(rx_data[8] | rx_data[9] << 8);
   axis_Z = static_cast<int16_t>(rx_data[10] | rx_data[11] << 8);
+
   left = rx_data[12] & 0x01;
   right = rx_data[13] & 0x01;
+
   key_W = (rx_data[14] >> 7) & 0x01;
   key_S = (rx_data[14] >> 6) & 0x01;
   key_A = (rx_data[14] >> 5) & 0x01;
