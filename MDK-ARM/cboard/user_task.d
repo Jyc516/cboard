29f3fa978a0bdf7ddcf61ec257597b80f59edf95
Jyc516 <jxxx17jyc@163.com>
2025-11-18 21:54:47 +0800

    feat: - MessageQueue completed

---
+++ MDK-ARM/cboard/user_task.d
@@ -0,0 +1,8 @@
+cboard/user_task.o: ..\UserCode\Src\user_task.cpp \
+  ..\UserCode\Inc\user_tasks.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\CMSIS_RTOS_V2\cmsis_os2.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\__config \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h
--- UserCode/Src/user_task.cpp
+++ UserCode/Src/user_task.cpp
@@ -5,23 +5,52 @@
 #include "user_tasks.h"
 #include "cmsis_os2.h"
 
-uint32_t count = 0;
+osMessageQueueId_t test_queue_handle;
+constexpr osMessageQueueAttr_t test_queue_attributes ={
+  .name = "test_queue",
+};
+
+
+
+uint32_t send = 0;
+uint32_t recv = 0;
 
 osThreadId_t test_task_handle;
-constexpr osThreadAttr_t testTask_attibutes = {
-  .name = "userTask",
-  .stack_size = 128 * 4,
+constexpr osThreadAttr_t test_task_attributes ={
+  .name = "test_task",
+  .stack_size = 256 * 4,
   .priority = osPriorityNormal,
 };
 
-void test_task(void *) {
+[[noreturn]]void test_task(void *) {
   while (true) {
     const auto tick = osKernelGetTickCount();
-    ++count;
-    osDelayUntil(tick + 1);
+    ++send;
+    osMessageQueuePut(test_queue_handle, &send, 0, 0);
+    osDelayUntil(tick + 100);
   }
 }
 
+
+
+osThreadId_t test1_task_handle;
+constexpr osThreadAttr_t test1_task_attributes ={
+  .name = "test_task",
+  .stack_size = 256 * 4,
+  .priority = osPriorityNormal,
+};
+
+[[noreturn]]void test1_task(void *) {
+  while (true) {
+    osMessageQueueGet(test_queue_handle, &recv, nullptr, osWaitForever);
+  }
+}
+
+
+
 void user_tasks_init() {
-  test_task_handle = osThreadNew(test_task, nullptr, &testTask_attibutes);
+  test_task_handle = osThreadNew(test_task, nullptr, &test_task_attributes);
+  test1_task_handle = osThreadNew(test1_task, nullptr, &test1_task_attributes);
+
+  test_queue_handle = osMessageQueueNew(16, sizeof(uint32_t), &test_queue_attributes);
 }
