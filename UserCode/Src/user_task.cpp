fb2bb1de928c6932bdba1adcba9e2b6c036f91b3
Jyc516 <jxxx17jyc@163.com>
2025-11-27 10:24:52 +0800

    fix: - Eventflags fixed.

--- UserCode/Src/user_task.cpp
+++ UserCode/Src/user_task.cpp
@@ -26,52 +26,97 @@ constexpr osEventFlagsAttr_t test_event_attributes = {
 
 
 
-uint32_t send = 0;
-auto flag_1 = 1u;
+// uint32_t send = 0;
+// auto flag_1 = 1u;
+//
+// osThreadId_t test_task_handle;
+// constexpr osThreadAttr_t test_task_attributes = {
+//   .name = "test_task",
+//   .stack_size = 256 * 4,
+//   .priority = osPriorityNormal,
+// };
+//
+// [[noreturn]]void test_task(void *) {
+//   while (true) {
+//     const auto tick = osKernelGetTickCount();
+//     if (++send % 5 == 0) {
+//       osSemaphoreRelease(test_semaphore_handle);
+//
+//       osEventFlagsSet(test_event_handle, flag_1);
+//     }
+//     // osMessageQueuePut(test_queue_handle, &send, 0, 0);
+//     osDelayUntil(tick + 100);
+//   }
+// }
+//
+//
+//
+// uint32_t recv = 0;
+// auto flag_2 = 1u << 1;
+//
+// osThreadId_t test1_task_handle;
+// constexpr osThreadAttr_t test1_task_attributes = {
+//   .name = "test_task",
+//   .stack_size = 256 * 4,
+//   .priority = osPriorityNormal,
+// };
+//
+// [[noreturn]]void test1_task(void *) {
+//   while (true) {
+//     // osMessageQueueGet(test_queue_handle, &recv, nullptr, osWaitForever);
+//     osSemaphoreAcquire(test_semaphore_handle, osWaitForever);
+//
+//     osEventFlagsSet(test_event_handle, flag_2);
+//     ++recv;
+//   }
+// }
 
-osThreadId_t test_task_handle;
-constexpr osThreadAttr_t test_task_attributes = {
-  .name = "test_task",
+
+
+
+
+constexpr auto flag_3 = 1u;
+osThreadId_t test3_task_handle;
+constexpr osThreadAttr_t test3_task_attributes = {
+  .name = "test3_task",
   .stack_size = 256 * 4,
   .priority = osPriorityNormal,
 };
 
-[[noreturn]]void test_task(void *) {
-  while (true) {
-    const auto tick = osKernelGetTickCount();
-    if (++send % 5 == 0) {
-      osSemaphoreRelease(test_semaphore_handle);
+[[noreturn]] void test3_task(void *) {
+  // osEventFlagsClear(test_event_handle, flag_3);
 
-      osEventFlagsSet(test_event_handle, flag_1);
-    }
-    // osMessageQueuePut(test_queue_handle, &send, 0, 0);
-    osDelayUntil(tick + 100);
+  while (true) {
+    osDelay(1000);
+    osEventFlagsSet(test_event_handle, flag_3);
   }
 }
 
 
 
-uint32_t recv = 0;
-auto flag_2 = 1u << 1;
+auto flag_4 = 1u << 1;
+uint32_t cnt4 = 0;
 
-osThreadId_t test1_task_handle;
-constexpr osThreadAttr_t test1_task_attributes = {
-  .name = "test_task",
+osThreadId_t test4_task_handle;
+constexpr osThreadAttr_t test4_task_attributes = {
+  .name = "test4_task",
   .stack_size = 256 * 4,
   .priority = osPriorityNormal,
 };
 
-[[noreturn]]void test1_task(void *) {
-  while (true) {
-    // osMessageQueueGet(test_queue_handle, &recv, nullptr, osWaitForever);
-    osSemaphoreAcquire(test_semaphore_handle, osWaitForever);
+[[noreturn]] void test4_task(void *) {
+  osEventFlagsClear(test_event_handle, flag_4);
 
-    osEventFlagsSet(test_event_handle, flag_2);
-    ++recv;
+  while (true) {
+    osDelay(1500);
+    cnt4 += 1;
+    osEventFlagsSet(test_event_handle, flag_4);
   }
 }
 
 
+
+uint32_t cnt = 0;
 
 osThreadId_t test2_task_handle;
 constexpr osThreadAttr_t test2_task_attributes = {
@@ -82,18 +127,23 @@ constexpr osThreadAttr_t test2_task_attributes = {
 
 [[noreturn]]void test2_task(void *) {
   while (true) {
-    osEventFlagsWait(test_event_handle, flag_1 | flag_2, osFlagsWaitAll, osWaitForever);
-    osEventFlagsClear(test_event_handle, flag_1 | flag_2);
+    osEventFlagsWait(test_event_handle, flag_3 | flag_4, osFlagsWaitAll, osWaitForever);
+    ++cnt;
+    // osEventFlagsClear(test_event_handle, flag_3 | flag_4);
   }
 }
 
 
 
 void user_tasks_init() {
-  test_queue_handle = osMessageQueueNew(16, sizeof(uint32_t), &test_queue_attributes);
-  test_semaphore_handle = osSemaphoreNew(1, 0, &test_semaphore_attributes);
+  // test_queue_handle = osMessageQueueNew(16, sizeof(uint32_t), &test_queue_attributes);
+  // test_semaphore_handle = osSemaphoreNew(1, 0, &test_semaphore_attributes);
 
-  test_task_handle = osThreadNew(test_task, nullptr, &test_task_attributes);
-  test1_task_handle = osThreadNew(test1_task, nullptr, &test1_task_attributes);
+  // test_task_handle = osThreadNew(test_task, nullptr, &test_task_attributes);
+  // test1_task_handle = osThreadNew(test1_task, nullptr, &test1_task_attributes);
   test2_task_handle = osThreadNew(test2_task, nullptr, &test2_task_attributes);
+  test3_task_handle = osThreadNew(test3_task, nullptr, &test3_task_attributes);
+  test4_task_handle = osThreadNew(test4_task, nullptr, &test4_task_attributes);
+
+  test_event_handle = osEventFlagsNew(&test_event_attributes);
 }
